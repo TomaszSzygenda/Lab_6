8a69d8f6424eec345945eca3ab13f637020ab12f
TomaszSzygenda <82755316+TomaszSzygenda@users.noreply.github.com>
2021-05-16 15:17:58 +0200

    Add files via upload

---
+++ Lab_6_zad_2_v2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+lista=input("podaj ciąg i zatwierdź:")
+
+for i in range (lista):
+    
+    for( int x=-4 <= n; x = 4 )
+    {
+        if( lista[ i ] % 2 == 0 )
+             cout << i << ", ";
+    
