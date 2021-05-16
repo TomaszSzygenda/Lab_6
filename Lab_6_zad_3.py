67110d4201e4217d4c894ae9d73b5dcf62d1e6e8
TomaszSzygenda <82755316+TomaszSzygenda@users.noreply.github.com>
2021-05-16 15:28:28 +0200

    Add files via upload

---
+++ Lab_6_zad_3.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+int f1(void)
+{
+puts("Jestem funkcją f1(), zwracam wartość 5.");
+return(5);
+}
+
+int f2(void)
+{
+puts("Jestem funkcją f2(), zwracam wartość 2.");
+return(2);
+}
+
+int f3(void)
+{
+puts("Jestem funkcją f3(), zwracam wartość 3.");
+return(3);
+}
+
+a=5
+b=2
+c=3
+   
+solution = (a,b,c) 
+ 
